bd227bbb4903fa664528a07c4682d892eba23c76
znzty <znzty@DESKTOP-JI43L4C>
2021-09-09 12:20:55 +0900

    8_2_ex1

--- src/main/java/com/javaGG/ex/ServletContext.java
+++ src/main/java/com/javaGG/ex/ServletContext.java
@@ -2,8 +2,6 @@ package com.javaGG.ex;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
-import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
